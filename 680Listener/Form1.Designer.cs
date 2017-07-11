137ca6dc40ec080a04b4bb27912534bd2bf28693
thutson <thutson79@gmail.com>
2017-07-11 13:07:10 -0400

    More channels
    Added a few more channels to the drop down.

--- 680Listener/Form1.Designer.cs
+++ 680Listener/Form1.Designer.cs
@@ -44,6 +44,7 @@
             // 
             // comboBox1
             // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.comboBox1.FormattingEnabled = true;
             this.comboBox1.Location = new System.Drawing.Point(197, 0);
             this.comboBox1.Name = "comboBox1";
--- 680Listener/Form1.cs
+++ 680Listener/Form1.cs
@@ -13,7 +13,9 @@ namespace _680Listener
 
             var items = new[] {
                 new { Text = "680 The Fan", Value = "680" },
-                new { Text = "1340 The Fan 3", Value = "1340" }
+                new { Text = "1230 The Fan 2", Value = "1230" },
+                new { Text = "1340 The Fan 3", Value = "1340" },
+                new { Text = "750 WSB AM", Value = "750" }
             };
 
             comboBox1.DataSource = items;
@@ -26,8 +28,12 @@ namespace _680Listener
         {
             if (comboBox1.SelectedValue.ToString() == "680")
                 webBrowser1.Navigate("http://player.tritondigital.com/15081");
-            else
+            else if (comboBox1.SelectedValue.ToString() == "1230")
+                webBrowser1.Navigate("http://player.tritondigital.com/19591");
+            else if (comboBox1.SelectedValue.ToString() == "1340")
                 webBrowser1.Navigate("http://player.tritondigital.com/19601");
+            else
+                webBrowser1.Navigate("http://player.streamguys.com/cmg/atl750/sgplayer/player.php");
         }
     }
 }
