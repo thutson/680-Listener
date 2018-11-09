a6b8532b362deb45445ce9ffb026e36fd8c2b912
thutson <thutson79@gmail.com>
2018-11-09 13:25:28 -0500

    Added southern sports today.

--- 680Listener/Form1.cs
+++ 680Listener/Form1.cs
@@ -15,6 +15,7 @@ namespace _680Listener
                 new { Text = "680 The Fan", Value = "680" },
                 new { Text = "1230 The Fan 2", Value = "1230" },
                 new { Text = "1340 The Fan 3", Value = "1340" },
+                new { Text = "Southern Sports Today", Value = "SST" },
                 new { Text = "750 WSB AM", Value = "750" }
             };
 
@@ -33,6 +34,8 @@ namespace _680Listener
                 webBrowser1.Navigate("http://player.tritondigital.com/19591");
             else if (comboBox1.SelectedValue.ToString() == "1340")
                 webBrowser1.Navigate("http://player.tritondigital.com/19601");
+            else if (comboBox1.SelectedValue.ToString() == "SST")
+                webBrowser1.Navigate("http://player.listenlive.co/57991/");
             else
                 webBrowser1.Navigate("http://player.streamguys.com/cmg/atl750/sgplayer/player.php");
         }
